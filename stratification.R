ab3ce47b587ec0ffce6210d09621bb82544e2625
CatherineD-hlab <cdellolio@mortonarb.org>
2025-12-30 09:58:48 -0600

    Add files via upload
    I edited this code to exclude trees in agricultural lands (or on the edges of agricultural lands) and to organize it a bit more efficiently (removing the older code at the beginning.)   This now creates the proposed sample.

--- stratification.R
+++ stratification.R
@@ -5,62 +5,121 @@
 #I call one datasheet: ILM_assoc_trees, which is just a datasheet of all the ILM trees.  Alternatively, this can be run with the full datasheet.csv filtered by site == "ILM".
 
 #setting working directory
+set.seed(1111)
 setwd("~/dataviz_cfd")
 df <- read.csv("ILM_assoc_trees.csv")
 library(dplyr)
 
 #filter to eligible butternuts: adults with DBH greater than 4.5 cm and less than 50 cm
 adults_df <- df %>% filter(seedling_or_adult == "Adult")
+adults_df <- adults_df %>% filter(number != "212" | number != "314" | number != "309" | number != "428" | number != "387" | number != "041")
 adults_df <- adults_df %>% filter(dbh_cm >= 4.5)
 adults_df <- adults_df %>% filter(dbh_cm <=50)
+#excluding trees in agricultural lands
+adults_df <- adults_df %>% filter(number != "193" | number != "424" | number != "425" | number != "119" | number != "120")
 
 #creating the subgroups:
 #health is represented by two metrics: girdle percentage and percent live canopy
 # girdle is considered high if it is over 60%, low if it is less than or equal to 30%, and medium if in between
-# percent live canopy is considered high if it is over 75%, low if it is less than or equal to 30%, and medium if in between
+# percent live canopy is considered high if it is over 70%, low if it is less than or equal to 30%, and medium if in between
 # these are independent metrics so they create a 3x3 grid of possible subgroups
-cat1 <- adults_df %>%  filter(adult_percent_live_canopy <= 30 & live_adult_girdle <= 30)
-cat2 <- adults_df %>%  filter(adult_percent_live_canopy <= 75 & adult_percent_live_canopy > 30 & live_adult_girdle <= 30)
-cat3 <- adults_df %>%  filter(adult_percent_live_canopy > 75 & live_adult_girdle <= 30)
-cat4 <- adults_df %>%  filter(adult_percent_live_canopy <= 30 & 30 < live_adult_girdle  & live_adult_girdle <= 60)
-cat5 <- adults_df %>%  filter(30 < adult_percent_live_canopy & adult_percent_live_canopy <= 75 & 30 < live_adult_girdle  & live_adult_girdle <= 60)
-cat6 <- adults_df %>%  filter(75 < adult_percent_live_canopy & 30 < live_adult_girdle  & live_adult_girdle <= 60)
-cat7 <- adults_df %>%  filter(adult_percent_live_canopy <= 30 & 60 < live_adult_girdle)
-cat8 <- adults_df %>%  filter(30 < adult_percent_live_canopy & adult_percent_live_canopy <= 75 & 60 < live_adult_girdle)
-cat9 <- adults_df %>%  filter(75 < adult_percent_live_canopy & 60 < live_adult_girdle)
+cat1 <- adults_df %>%  filter(adult_percent_live_canopy <= 70 & live_adult_girdle <= 50) #low canopy low girdle
+cat2 <- adults_df %>%  filter(adult_percent_live_canopy <= 70 & live_adult_girdle > 50) #low canopy high girdle
+cat3 <- adults_df %>%  filter(adult_percent_live_canopy > 70 & live_adult_girdle > 50) #high canopy low girdle
+cat4 <- adults_df %>%  filter(adult_percent_live_canopy > 70 & live_adult_girdle <= 50) #high canopy high girdle
+scatterplot(adults_df, adult_percent_live_canopy, live_adult_girdle, "df")
 
-#retrieving the list of tree IDs in each subgroup
-cat1$number
-cat2$number
-cat3$number
-cat4$number
-cat5$number
-cat6$number
-cat7$number
-cat8$number
-cat9$number
+ggplot() +
+  geom_point(data = cat1, aes(gps_w, gps_n), colour="green2") +
+  geom_point(data = cat2, aes(gps_w, gps_n), colour="red") +
+  geom_point(data = cat3, aes(gps_w, gps_n), colour="yellow") +
+  geom_point(data = cat4, aes(gps_w, gps_n), colour="blue") +
+  ggtitle("Map of all eligible adults by category")
+#blue (high canopy and low girdle) conains a lot of the extremities and also many in middle
 
 #a function to sample a certain size from each category.  the reason I don't have it sample 6 from every category is because category 1 is too small--only 2 trees.  So those will both be sampled.
 randomsample <- function(cat, size){
   s <-  sample(1:nrow({{cat}}), size, replace = FALSE)
   return(cat[s,])
 }
-r1 <- randomsample(cat1, 2)
-r2 <- randomsample(cat2, 6)
-r3 <- randomsample(cat3, 6)
-r4 <- randomsample(cat4, 6)
-r5 <- randomsample(cat5, 6)
-r6 <- randomsample(cat6, 6)
-r7 <- randomsample(cat7, 6)
-r8 <- randomsample(cat8, 6)
-r9 <- randomsample(cat9, 6)
-#combine every selection from the subgroups to make the sample
-totalsample <- rbind(r1, r2,r3, r4, r5, r6, r7, r8, r9)
+
+#we need to figure out a way to have the computer only select trees that are more than about 10-15 meters apart
+#shout out rodrigo on stack overflow for this function: https://stackoverflow.com/questions/639695/how-to-convert-latitude-or-longitude-to-meters
+measure <- function(lon1,lat1,lon2,lat2) {
+  R <- 6378.137                                # radius of earth in Km
+  dLat <- (lat2-lat1)*pi/180
+  dLon <- (lon2-lon1)*pi/180
+  a <- (sin(dLat/2))^2 + cos(lat1*pi/180)*cos(lat2*pi/180)*(sin(dLon/2))^2
+  c <- 2 * atan2(sqrt(a), sqrt(1-a))
+  d <- R * c
+  return (d * 1000)                            # distance in meters
+}
+
+#RUN THIS ONCE. It resets the sample back to nothing.
+totalsample <- data.frame()
+adults_remaining <- adults_df
+
+#RUN FROM HERE TO DASHED LINE AS MANY TIMES AS NEEDED TO CREATE A SAMPLE OF THE DESIRED SIZE (in my case, 13 times to get a sample of 52).  This builds the sample by selecting one tree from each category, checking to exclude any very nearby trees to the selected one that now would be spatially autocorrelated with it, and filtering the remaining trees to only the ones that are still eligible (that is, not too close to any of the previously selected trees).
+set.seed(1111)
+t1 <- randomsample(cat1, 1)
+adults_cycle <- data.frame()
+for (i in 1:nrow(adults_remaining)){
+  if (measure(t1$gps_w, t1$gps_n, adults_remaining[i,]$gps_w, adults_remaining[i,]$gps_n) > 10){
+    adults_cycle <- rbind(adults_cycle, adults_remaining[i,])
+  }
+}
+adults_remaining <- adults_cycle
+totalsample <- rbind(totalsample, t1)
+set.seed(1111)
+t1 <- randomsample(cat2, 1)
+adults_cycle <- data.frame()
+for (i in 1:nrow(adults_remaining)){
+  if (measure(t1$gps_w, t1$gps_n, adults_remaining[i,]$gps_w, adults_remaining[i,]$gps_n) > 10){
+    adults_cycle <- rbind(adults_cycle, adults_remaining[i,])
+  }
+}
+adults_remaining <- adults_cycle
+totalsample <- rbind(totalsample, t1)
+set.seed(1111)
+t1 <- randomsample(cat3, 1)
+adults_cycle <- data.frame()
+for (i in 1:nrow(adults_remaining)){
+  if (measure(t1$gps_w, t1$gps_n, adults_remaining[i,]$gps_w, adults_remaining[i,]$gps_n) > 10){
+    adults_cycle <- rbind(adults_cycle, adults_remaining[i,])
+  }
+}
+adults_remaining <- adults_cycle
+totalsample <- rbind(totalsample, t1)
+set.seed(1111)
+t1 <- randomsample(cat4, 1)
+adults_cycle <- data.frame()
+for (i in 1:nrow(adults_remaining)){
+  if (measure(t1$gps_w, t1$gps_n, adults_remaining[i,]$gps_w, adults_remaining[i,]$gps_n) > 10){
+    adults_cycle <- rbind(adults_cycle, adults_remaining[i,])
+  }
+}
+adults_remaining <- adults_cycle
+totalsample <- rbind(totalsample, t1)
+ggplot() +
+  geom_point(data = adults_df, aes(gps_w, gps_n), colour="yellow2") +
+  geom_point(data = adults_remaining, aes(gps_w, gps_n), colour="green2") +
+  geom_point(data = totalsample, aes(gps_w, gps_n), colour="red") +
+  ggtitle("Remaining eligible adults (green) with sample (red) and excluded autocorrelated trees (yellow)")
+
+cat1 <- adults_remaining %>%  filter(adult_percent_live_canopy <= 70 & live_adult_girdle <= 50) #low canopy low girdle (dying of other causes)
+cat2 <- adults_remaining %>%  filter(adult_percent_live_canopy <= 70 & live_adult_girdle > 50) #low canopy high girdle (dying of canker)
+cat3 <- adults_remaining %>%  filter(adult_percent_live_canopy > 70 & live_adult_girdle > 50) #high canopy high girdle (surviving despite canker)
+cat4 <- adults_remaining %>%  filter(adult_percent_live_canopy > 70 & live_adult_girdle <= 50) # high canopy low girdle (spared from canker)
+#---------------------------------------------------------------------------
+#END OF CHUNK TO RUN MULTIPLE TIMES. Run as one big block 13 times to recreate my sample (the set seed will ensure you create the same sample every time!)
+
 #retrieve tree IDs for the proposed sample
 totalsample$number
 #This gives me the list of trees to sample at ILM for the associate trees analysis!
 
+#------------------------------
 #TESTING FOR REPRESENTATIVENESS
+#------------------------------
 #does this sample consist of mostly similar proportions of trees in each crown class as the eligible population?  Trees of different crown classes receive different amounts of light which might really impact our analysis.
 barplot <- function(data, variable, title){
   ggplot(data, aes({{variable}})) +
@@ -69,16 +128,7 @@ barplot <- function(data, variable, title){
 }
 barplot(totalsample, adult_crown_class, "crown class of proposed sample")
 barplot(adults_df, adult_crown_class, "crown class of total pool of eligible adults")
-#the answer is mostly yes (though if you re-run this code, you will get a different sample!)
-barplot(r1, adult_crown_class, "r1 crown classes")
-barplot(r2, adult_crown_class, "r2 crown classes")
-barplot(r3, adult_crown_class, "r3 crown classes")
-barplot(r4, adult_crown_class, "r4 crown classes")
-barplot(r5, adult_crown_class, "r5 crown classes")
-barplot(r6, adult_crown_class, "r6 crown classes")
-barplot(r7, adult_crown_class, "r7 crown classes")
-barplot(r8, adult_crown_class, "r8 crown classes")
-barplot(r9, adult_crown_class, "r9 crown classes")
+#the answer is definitely yes
 #does the sample appear to cover the spatial extent of eligible trees?
 ggplot() +
   geom_point(data = adults_df, aes(gps_w, gps_n), colour="green2") +
